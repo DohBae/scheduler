bb7768f275646439872a13c92496a3d53ca758aa
DohBae <bdoherty92@msn.com>
2022-09-07 14:13:26 -0400

    Build InterviewerListItem component. Add conditional CSS

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,25 @@
 import React from "react";
+import classNames from "classnames";
 
-export default function InterviewerListItem() {
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  // console.log(props)
+  // console.log(props.name)
+  // console.log(props.avatar)
+  // console.log(props.id)
+  let interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
   <img
     className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
+    src={props.avatar}
+    alt={props.name}
   />
-  Sylvia Palmer
+  {props.name}
 </li>
   );
 }
