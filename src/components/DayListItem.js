a97fef9654159221446966b96d5056cf737044ce
DohBae <bdoherty92@msn.com>
2022-09-06 20:27:50 -0400

    Add item that allows days that are selected to be highlighted. Add component that allows user to see data pertaining to selected week day

---
+++ src/components/DayListItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classNames from "classnames";
+
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+  
+console.log(props)
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2>{props.name}</h2>
+      {props.spots > 1 &&  <h3>{props.spots} spots remaining</h3>}
+      {props.spots === 1 &&  <h3>{props.spots} spot remaining</h3>}
+      {props.spots === 0 &&  <h3>no spots remaining</h3>}
+    </li>
+  );
+}
+
+// li represents entire day item
+// h2 displays the name
+// h3 display spots remaining for the day
