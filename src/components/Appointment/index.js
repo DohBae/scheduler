a389a8f888a0b041a1aef4d2640f8d15a37c77c6
DohBae <bdoherty92@msn.com>
2022-09-07 21:41:44 -0400

    Build appointment empty and appointment booked components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,16 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
+  console.log(props.time)
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? <Show /> : <Empty />}
+    </article>
   );
 }
