17350d0271ee2ad6e85d40746bf08202df34cf97
DohBae <bdoherty92@msn.com>
2022-09-07 21:24:57 -0400

    Reset enter student name form when cancel button is pressed

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -6,17 +6,20 @@ import "components/InterviewerList.scss";
 export default function InterviewerList(props) {
   const interviewers = props.interviewers;
 
+  console.log("INTERVIEWER", interviewers)
+
   const interviewersArray = interviewers.map( interviewer => {
     const isSelected = interviewer.id === props.value
     
-    return <InterviewerListItem 
+    return ( <InterviewerListItem 
     key={interviewer.id} 
     id={interviewer.id} 
     name ={interviewer.name} 
     avatar={interviewer.avatar} 
     selected={isSelected} 
-    setInterviewer={() => props.onChange(interviewer.id)}/>
+    setInterviewer={() => props.onChange(interviewer.id)}/>)
   })
+  
 
   return (
     <section className="interviewers">
